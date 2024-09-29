79d093ad54889bd62f54cce841ad9dc071c26e15
JEJS-IA97 <jose.e.jimenez.1411@gmail.com>
2024-09-29 13:07:26 -0400

    Update

--- social-media-dashboard/src/components/Header.jsx
+++ social-media-dashboard/src/components/Header.jsx
@@ -21,12 +21,12 @@ export const Header = () => {
         <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 mb-[940px] md:mb-[460px] xl:mb-[177px]">
             <div className="md:max-w-[720px] xl:max-w-[1110px] max-w-[326px] place-content-center absolute left-0 right-0 mx-auto xl:flex xl:justify-between xl:items-center">
             <div className="xl:flex xl:flex-col xl:">
-            <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1">Social Media Dashboard</h1>
-            <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6 xl:mb-0">Total Followers: 23,004</p>
+            <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue xl:text-[30px] text-[25px] font-bold mb-1">Social Media Dashboard</h1>
+            <p className="text-Dark-Grayish-Blue xl:text-[14px] text-[16px] dark:text-Desaturated-Blue font-bold mb-6 xl:mb-0">Total Followers: 23,004</p>
             </div>
             <hr className="bg-black mb-[19px] xl:hidden"/>
-            <div className="flex justify-between xl:justify-end xl:gap-5">
-                <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold">Dark Mode</p>
+            <div className="flex justify-between xl:justify-end xl:gap-3">
+                <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold xl:text-[14px] text-[16px]">Dark Mode</p>
                 <label htmlFor="darkmode" className="border relative bg-Toggle w-12 h-6 rounded-full overflow-hidden cursor-pointer flex items-center pl-[3px]">
                      <input onClick={handleClick} id="darkmode" type="checkbox" className="peer sr-only"/>
                      <div className="w-full h-full peer-checked:bg-Toggle-Gradient absolute top-0 left-0"></div>
